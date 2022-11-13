750ab49d5a4bbd5a04951e9678873a8acdcd65a7
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-11-13 09:52:04 +0530

    Checking in Panildrome Pattern with Numbers

---
+++ Panildrom Pattern with Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panildrom_Pattern_with_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= (n - i); j++)
+                    Console.Write(" ");
+
+                int flag = i;
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(flag);
+                    flag--;
+                }
+                flag = 2;
+                for (int j = 1; j <= (i - 1); j++)
+                {
+                    Console.Write(flag);
+                    flag++;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
