3bb7c1aac2bb4a0a48b20e80041089342385a9de
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-11-13 09:25:38 +0530

    Checking in Number Pyramid Pattern

---
+++ Number Pyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n-i; j++)
+                    Console.Write(" ");
+                int count = 1;
+
+                while(count<=(i))
+                {
+                    Console.Write(i+ " ");
+                    count++;
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
