0f58768f36ec6666a94410ba6950efebe72ac7e5
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-11-16 11:02:14 +0530

    Checking in Kunal DSA Question No 35

---
+++ Kunal DSA Pattern No 35/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kunal_DSA_Pattern_No_35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j);
+                for (int j = 1; j <= 2*(n-i); j++)
+                    Console.Write(" ");
+                for (int j = i; j >= 1; j--)
+                    Console.Write(j);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
